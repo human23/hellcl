60a6ef63b48528f24f1099829d0c4dfc0255657d
Human23 <159918736+human23@users.noreply.github.com>
2026-05-23 19:52:38 +0200

    Implement mouse drawing and click interaction
    Added mouse drawing and click feedback functionality.

--- hello.c
+++ hello.c
@@ -2,22 +2,84 @@
 #include <stdlib.h>
 #include <raylib.h>
 
-int main(void)
+void DrawMouse()
 {
+  // Draw Mouse
+  DrawRectangle(GetMouseX(), GetMouseY(), 6, 10, BLUE);
+}
+
+void Click()
+
+{
+        DrawText("CLICK", GetScreenWidth() -90, GetScreenHeight()-25, 20, BLACK);
+}
+
+
+void main()
+{
+
   InitWindow(800, 600, "hello");
-  SetTargetFPS(60);
+
+  int FPS = 165; SetTargetFPS(FPS);
+
+  //buttonthingies
+  Rectangle buttonthingies = { 300, 250, 200, 100 };
 
 
+
+  Rectangle Menu = {20, 20, GetScreenHeight()/2+GetScreenHeight()/4,GetScreenWidth()/2+GetScreenWidth()/5};
+
+
+  Rectangle MenuButton = { GetScreenHeight(), 250, 100, 50 };
+  bool ShowMenu = false;
+
+
+  HideCursor();
+
+
+  //update each frame
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("beans?", 10, 10, 20, BLACK);
+    DrawText("RAYLIB BASED C BASED LINUX BASED ARCH BASED CACHYOS BASED OPERATING-SYSTEM/LINUXDISTRO", 10, 10, 5, BLACK);
+
+    DrawRectangleRec(buttonthingies, RED);
     Image genImage = GenImageColor(100, 100, RED);
+
+
+    //Debugging Mouse Button
+    DrawRectangleRec(buttonthingies, RED);
+    if (CheckCollisionPointRec(GetMousePosition(), buttonthingies) && IsMouseButtonPressed(0))
+    {
+      Click();
+    }
+    //Menu
+    DrawRectangleRec(MenuButton, GRAY);
+    if (CheckCollisionPointRec(GetMousePosition(), MenuButton) && IsMouseButtonPressed(0))
+    {ShowMenu = !ShowMenu;
+      Click();
+    }
+
+    if (ShowMenu == true)
+    {DrawRectangleRec(Menu, GRAY);};
+
+
+    DrawMouse();
+
+
+
     EndDrawing();
+    UnloadImage(genImage);
   }
 
+
+
+
+
+
   CloseWindow();
+  ShowCursor();
   exit(EXIT_SUCCESS);
 }
 
